861ef29e1cacf17bde42843e4b5dc8809808cff4
cassiano yasumitsu <cassiano@usuarioincomum.com>
2021-08-29 16:34:05 +0900

    functions

--- main.go
+++ main.go
@@ -3,6 +3,12 @@ package main
 import "fmt"
 
 func main() {
-	var card string = "Ace of Spades"
+	// var card string = "Ace of Spades"
+	card := newCard()
+
 	fmt.Println(card)
+}
+
+func newCard() string {
+	return "Five of Diamonds"
 }
