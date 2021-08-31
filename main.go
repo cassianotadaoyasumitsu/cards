4c8f09f554ce42028d707b0f6599d1d1a5926669
cassiano yasumitsu <cassiano@usuarioincomum.com>
2021-08-31 09:57:09 +0900

    []byte

--- main.go
+++ main.go
@@ -1,10 +1,13 @@
 package main
 
 func main() {
+	// cards := newDeck()
+
+	// hand, remainingCards := deal(cards, 5)
+
+	// hand.print()
+	// remainingCards.print()
+
 	cards := newDeck()
-
-	hand, remainingCards := deal(cards, 5)
-
-	hand.print()
-	remainingCards.print()
+	cards.saveToFile("my_cards")
 }
